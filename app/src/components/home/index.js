b297d7b21e41a85ccaec41ccb6ffbf058679e110
nahuelkbx <nahueltomasmonserrat@gmail.com>
2024-07-12 10:46:58 -0300

    add routes with react-router-dom

---
+++ app/src/components/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div id='home'>
+        
+    </div>
+  )
+}
+
+export default Home
--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Root from "./routes/root";
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './components/home/index'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        path: "/items",
+        element: <Home/>
+      }
+    ]
+  },
+  
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
